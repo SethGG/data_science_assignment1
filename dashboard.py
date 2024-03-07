27efc3d82019b80f0badb2fa5bff892b982e9d47
Marts Niewandt <m.r.niewandt@hotmail.com>
2024-03-07 10:55:57 +0100

    Implemented Segmentation and filtering basics, creates bar chart based on SKU ID

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,8 @@
 from preprocessing import sales, crashes, ratings
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
 
 print(sales)
 print(crashes)
@@ -13,11 +15,45 @@ print(ratings)
 output_file("vis1.html")
 fig = figure()
 
-show(fig)
+#show(fig)
 
 # Attribute Segmentation and Filtering: Present sales volume (as above) segmented per
 # attribute: at least the SKU id (in-app purchase option) attribute should be included, but you
 # can also think of the day of the week, time of the day or the country of the customer.
+
+'''fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
+counts = [5, 3, 4, 2, 4, 6]
+output_file("vis1.html")
+p = figure(x_range=fruits, height=350, title="Fruit Counts",
+           toolbar_location=None, tools="")
+
+p.vbar(x=fruits, top=counts, width=0.9)
+
+p.xgrid.grid_line_color = None
+p.y_range.start = 0
+
+show(p)'''
+
+
+lenpre = len(sales[sales['Sku Id']=='premium'])
+lenchar = len(sales[sales['Sku Id']=='unlockcharactermanager'])
+#lencountry = len(sales[sales['Sku Id']==''])
+print(sales["Buyer Country"].unique())
+#for col in sales.columns:
+#    print(col)
+print(lenpre)
+print(lenchar)
+print(lenpre+lenchar)
+x=['premium','unlockcharactermanager']
+y=[lenpre,lenchar]
+
+fig = figure(x_range=x, title="SKU ID", toolbar_location=None, tools="")
+
+fig.vbar(x=x, bottom=0, top=y, color='blue', width=0.9, legend_label='SKU ID')
+fig.y_range.start = 0
+fig.legend.location = 'top_left'
+
+show(fig)
 
 # TO-DO
 
@@ -34,3 +70,10 @@ show(fig)
 # again to give management as much geographic insight as possible.
 
 # TO-DO
+
+#output_file('filename.html', title='Empty Bokeh Figure')
+
+#fig = figure(background_fill_color='gray', background_fill_alpha=0.5, border_fill_color='blue', border_fill_alpha=0.25, #plot_height=300, plot_width=500, x_axis_label='X Label',
+ #x_axis_type='datetime', x_axis_location='above', x_range=('2018-01-01', '2018-06-30'), y_axis_label='Y Label', y_axis_type='linear', y_axis_location='left',
+ #y_range=(0, 100), title='Example Figure', title_location='right', toolbar_location='below', tools='save')
+#show(fig)
