380b3fa38ab340e9f606368f93ce8fa1b47cfe22
dzee <df.zee@hotmail.com>
2024-03-09 20:02:36 +0100

    sales vol nu per week

--- sales_vol.py
+++ sales_vol.py
@@ -1,9 +1,10 @@
 from preprocessing import sales
-from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.models import ColumnDataSource, HoverTool, FixedTicker, DatetimeTickFormatter, PrintfTickFormatter
 from bokeh.io import output_file
 from bokeh.plotting import figure, show
 from bokeh.layouts import column
 import pandas as pd
+import numpy as np
 
 # Sales Volume: Visualize the sales over time (for example, per month or per day) in
 # terms of at least two measures. For example: real money (Amount) and transaction count
@@ -12,7 +13,7 @@ import pandas as pd
 sales_premium = sales[sales["Sku Id"] == "premium"]
 sales_unlock = sales[sales["Sku Id"] == "unlockcharactermanager"]
 
-daily_grouper = pd.Grouper(key="Datetime", freq="D")
+daily_grouper = pd.Grouper(key="Datetime", freq="w-MON")
 
 daily_premium = sales_premium.groupby(daily_grouper).agg(
     {"Sku Id": "count"}).rename(columns={"Sku Id": "premium"})
@@ -21,52 +22,60 @@ daily_unlock = sales_unlock.groupby(daily_grouper).agg(
 daily_money = sales.groupby(daily_grouper).agg({"Amount (Merchant Currency)": "sum"})
 
 daily_summary = daily_money.join([daily_premium, daily_unlock])
+daily_summary.index = daily_summary.index - pd.Timedelta(weeks=1)
 print(daily_summary)
 
 source = ColumnDataSource(daily_summary)
 
-start_date = daily_summary.index[0]
-end_date = start_date + pd.DateOffset(days=30)
-
 output_file("vis1.html")
 
 fig1 = figure(
-    x_axis_type="datetime",
+    title="Weekly Transactions",
     height=300,
     width=800,
-    x_range=(start_date, end_date),
-    y_range=(0, 25),
-    x_axis_label="Date",
+    y_range=(0, 100),
+    x_axis_label="Week",
     y_axis_label="Transactions",
-    tools=["xpan", "reset", "save", "xwheel_zoom"]
+    tools=["xpan", "reset", "save", "xwheel_zoom"],
+    background_fill_color="#fafafa"
 )
+fig1.xaxis.ticker = FixedTicker(ticks=daily_summary.index.astype(np.int64) // 10**6)
+fig1.xaxis.formatter = DatetimeTickFormatter(days="%U")
 
 products = ["premium", "unlockcharactermanager"]
 fig1.vbar_stack(stackers=products,
                 x="Datetime",
                 source=source,
-                width=(24*60*60*1000)*.9,
+                width=(7*24*60*60*1000)*.9,
                 color=["#718dbf", "#e84d60"],
                 legend_label=products)
+
+fig1.line(x="Datetime", y="Amount (Merchant Currency)", color="black", source=source)
+
 fig1.legend.location = "top_left"
 fig1.legend.orientation = "horizontal"
 
-fig1.add_tools(HoverTool(tooltips=[('Date', '@Datetime{%F}'), ('Sku Id', '$name'), ('Transactions', '@$name')],
+fig1.add_tools(HoverTool(tooltips=[('Date', '@Datetime{%Y-W%U}'), ('Sku Id', '$name'), ('Transactions', '@$name')],
                          formatters={'@Datetime': 'datetime'}))
 
 fig2 = figure(
-    x_axis_type="datetime",
+    title="Weekly Revenue",
     height=300,
     width=800,
-    x_range=(start_date, end_date),
-    y_range=(0, 120),
-    x_axis_label="Date",
-    y_axis_label="Amount",
-    tools=["xpan", "reset", "save", "xwheel_zoom"]
+    y_range=(0, 500),
+    x_axis_label="Week",
+    y_axis_label="Revenue",
+    tools=["xpan", "reset", "save", "xwheel_zoom"],
+    background_fill_color="#fafafa"
 )
+fig2.xaxis.ticker = fig1.xaxis.ticker
+fig2.xaxis.formatter = fig1.xaxis.formatter
+
+fig2.yaxis.formatter = PrintfTickFormatter(format="€ %s")
+
 fig2.line(x="Datetime", y="Amount (Merchant Currency)", color="black", source=source)
 
-fig2.add_tools(HoverTool(tooltips=[('Date', '@Datetime{%F}'), ('Amount', '@{Amount (Merchant Currency)}')],
+fig2.add_tools(HoverTool(tooltips=[('Date', '@Datetime{%Y-W%U}'), ('Revenue', '@{Amount (Merchant Currency)}')],
                          formatters={'@Datetime': 'datetime'}))
 
 grid = column(fig1, fig2)
