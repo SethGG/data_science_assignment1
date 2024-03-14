092f00dd2b7452117c6602612d964b211882a2db
dzee <df.zee@hotmail.com>
2024-03-14 15:29:45 +0100

    klaar

--- attribute_seg.py
+++ attribute_seg.py
@@ -1,16 +1,12 @@
-from preprocessing import sales, crashes#, ratings
-from bokeh.io import output_file
-from bokeh.plotting import figure, show, save
+from preprocessing import sales
+from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, Range1d, Plot, AnnularWedge, Legend, LegendItem, HoverTool, TapTool
-from bokeh.layouts import row, column, gridplot
-from bokeh.palettes import Category20c, viridis, linear_palette, Turbo256
 from math import pi
-from bokeh.transform import cumsum
-import pandas as pd
 
 # Attribute Segmentation and Filtering: Present sales volume (as above) segmented per
 # attribute: at least the SKU id (in-app purchase option) attribute should be included, but you
 # can also think of the day of the week, time of the day or the country of the customer.
+
 
 def get_uniques(datfram, column, uniques):
     tmplist = []
@@ -18,76 +14,67 @@ def get_uniques(datfram, column, uniques):
         tmplist.append(len(datfram[datfram[str(column)].dt.day_name() == str(x)]))
     return tmplist
 
-select_tools = ['tap','box_select','reset','save']
+
+select_tools = ['tap', 'box_select', 'reset', 'save']
 lenpre = len(sales[sales['Sku Id'] == 'premium'])
 lenchar = len(sales[sales['Sku Id'] == 'unlockcharactermanager'])
 x = ['premium', 'unlockcharactermanager']
 y = [lenpre, lenchar]
 
-fig = figure(x_range=x, title="Total sales by SKU ID", toolbar_location=None, tools=select_tools,x_axis_label="SKU ID",
-    y_axis_label="Transactions", background_fill_color="#fafafa")
+fig = figure(x_range=x, title="Total sales by SKU ID", toolbar_location=None, tools=select_tools, x_axis_label="SKU ID",
+             y_axis_label="Transactions", background_fill_color="#fafafa", height=500, width=400)
 
 fig.vbar(x=x, bottom=0, top=y, width=0.75, color=["#718dbf", "#e84d60"])
 fig.y_range.start = 0
 
+fig.add_tools(HoverTool(tooltips=[('Sku Id', '@x'), ('Sales', '@top')]))
+
 colors = {
-    'premium' :"#718dbf",
-    'unlockcharactermanager' : "#e84d60"
+    'premium': "#718dbf",
+    'unlockcharactermanager': "#e84d60"
 }
 
 xdr = Range1d(start=-2, end=2)
 ydr = Range1d(start=-2, end=2)
 
-donut = Plot(x_range=xdr, y_range=ydr, background_fill_color="#fafafa")
+donut = Plot(x_range=xdr, y_range=ydr, background_fill_color="#fafafa", height=500, width=500)
 donut.title.text = "Total sales by SKU ID"
 aggregated = sales.groupby("Sku Id").size().to_frame("count1")
-#print(aggregated)
 aggregated['proportion'] = aggregated['count1']/aggregated['count1'].sum()
 selected = aggregated[aggregated.proportion >= 0].copy()
 sku_id = selected.index.tolist()
 angles = selected.proportion.map(lambda x: 2*pi*(x)).cumsum().tolist()
 
 skuid_source = ColumnDataSource(dict(
-    start  = [0] + angles[:-1],
-    end    = angles,
-    colors = [colors[skuid] for skuid in x],
-    proportion = aggregated['proportion'],
-    count1 = aggregated['count1'],
-    skuid = aggregated.index
+    start=[0] + angles[:-1],
+    end=angles,
+    colors=[colors[skuid] for skuid in x],
+    proportion=aggregated['proportion'],
+    count1=aggregated['count1'],
+    skuid=aggregated.index
 ))
 
-glyph = AnnularWedge(x=0, y=0, inner_radius=0.9, outer_radius=1.75,
+glyph = AnnularWedge(x=0, y=0, inner_radius=0.9, outer_radius=1.7,
                      start_angle="start", end_angle="end",
                      line_color="white", line_width=3, fill_color="colors")
-r= donut.add_glyph(skuid_source, glyph)
+r = donut.add_glyph(skuid_source, glyph)
 
 legend = Legend(location="center")
 for i, name in enumerate(colors):
     legend.items.append(LegendItem(label=name, renderers=[r], index=i))
 donut.add_layout(legend, "center")
-donut.add_tools(HoverTool(tooltips=[('Sku ID','@skuid'),('Sales', '@count1'),('Proportion', '@proportion{00.0%}')]))
+donut.add_tools(HoverTool(tooltips=[('Sku ID', '@skuid'), ('Sales', '@count1'), ('Proportion', '@proportion{00.0%}')]))
 donut.add_tools(TapTool())
 
 weekday = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-#print(sales)
 sales_weekday = get_uniques(sales, "Datetime", weekday)
-#print(sales_weekday)
-#print(sales["Datetime"].dt.day_name())
 
-dayplot = figure(x_range=weekday, title="Sales per weekday", toolbar_location=None, tools=select_tools,x_axis_label="Weekday",
-    y_axis_label="Transactions", background_fill_color="#fafafa")
 
-dayplot.vbar(x=weekday, bottom=0, top=sales_weekday, color='blue', width=0.9)#, legend_label='Weekdays')
+dayplot = figure(x_range=weekday, title="Sales per weekday", toolbar_location=None,
+                 tools=select_tools, x_axis_label="Weekday",
+                 y_axis_label="Transactions", background_fill_color="#fafafa", height=500, width=500)
+
+dayplot.vbar(x=weekday, bottom=0, top=sales_weekday, color='grey', width=0.9)
 dayplot.y_range.start = 0
-#dayplot.legend.location = 'top_left'
-
-#fig3 = column(sku_id_countries, dayplot)
-
-#save(fig3)
-#fig4 = gridplot([[fig, dayplot]], toolbar_location='right')
-#show(fig4)
-fig5 = gridplot([[donut,fig]],toolbar_location='right')
-#show(column(fig5,dayplot))
-fig3 = column(fig5,dayplot)
-
+dayplot.add_tools(HoverTool(tooltips=[('Weekday', '@x'), ('Sales', '@top')]))
--- layout.py
+++ layout.py
@@ -1,4 +1,5 @@
 import sales_vol
+import attribute_seg
 import ratings_stability
 import geographical_dev
 from bokeh.layouts import column, row
@@ -12,7 +13,17 @@ stylesheet = ImportedStyleSheet(url="https://cdn.jsdelivr.net/npm/bulma@0.9.4/cs
 
 html = """
 <section class="section">
-  <h1 class="title">Sales Volume</h1>
+  <h1 class="title is-2">Emerald-IT: Complete Reference for Dungeons and Dragons 5</h1>
+  <h2 class="subtitle">
+    Interactive dashboard used to understand and interpret the data of the app.
+  </h2>
+  <h2 class="subtitle is-6 is-italic">
+    Data Science 2023-2024<br>
+    Assignment 1: visual analytics<br>
+    Daniël Zee (s2063131) & Marts Niewandt (s2624893)
+</section>
+<section class="section">
+  <h1 class="title is-4">Sales Volume</h1>
   <h2 class="subtitle">
     Here we can view the weekly sales over time in terms of the number of transactions and real money.
   </h2>
@@ -24,7 +35,19 @@ sales_layout = column(title, row(sales_vol.fig1, sales_vol.fig2))
 
 html = """
 <section class="section">
-  <h1 class="title">Ratings vs Stability</h1>
+  <h1 class="title is-4">Attribute Segmentation and Filtering</h1>
+  <h2 class="subtitle">
+    Here we can view the sales volume segmented per SKU id and day of the week.
+  </h2>
+</section>
+"""
+title = Div(text=html, stylesheets=[stylesheet])
+
+seg_layout = column(title, row(attribute_seg.donut, attribute_seg.fig, attribute_seg.dayplot))
+
+html = """
+<section class="section">
+  <h1 class="title is-4">Ratings vs Stability</h1>
   <h2 class="subtitle">
     Here we can compare the weekly average daily rating against the weekly average daily number of crashes.
   </h2>
@@ -36,7 +59,7 @@ rating_layout = column(title, row(ratings_stability.fig1, ratings_stability.fig2
 
 html = """
 <section class="section">
-  <h1 class="title">Geographical Development</h1>
+  <h1 class="title is-4">Geographical Development</h1>
   <h2 class="subtitle">
     Here we can view the sales volume and ratings per country in a geographical setting.
   </h2>
@@ -46,5 +69,5 @@ title = Div(text=html, stylesheets=[stylesheet])
 
 geo_layout = column(title, geographical_dev.fig)
 
-dashboard = column(sales_layout, rating_layout, geo_layout)
+dashboard = column(sales_layout, seg_layout, rating_layout, geo_layout)
 show(dashboard)
